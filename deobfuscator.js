7cefd338f82b62ee34742d61b12aa01b75c8a2eb
kisame172009-crypto <kisame172009@gmail.com>
2026-06-10 12:43:43 +0200

    Create deobfuscator.js

---
+++ deobfuscator.js
@@ -0,0 +1,6 @@
+// Désobfuscateur basique (à améliorer)
+module.exports = async function deobfuscate(code) {
+    // Pour l'instant, on renvoie le code
+    // TODO: Ajouter la logique de désobfuscation réelle
+    return code;
+};
